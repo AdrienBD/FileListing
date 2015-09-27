e47cbbf7132bd9f04aae2488442f9b9a43508901
adrienbd <adrienbd1@gmail.com>
2015-09-27 10:42:26 +0200

    Exporting list to said file

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -2,7 +2,11 @@ package main;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Observable;
 
 import javax.swing.JFrame;
@@ -38,12 +42,29 @@ public class Controller extends Observable {
 	}
 	
 	public void launchProcess() {
-		
+		File[] files = toListDirectory.listFiles();
+		exportFileList(files);
 	}
 	
 	private void updateLaunchable() {
 		launchable = this.exportFile != null && this.toListDirectory != null;
 		this.setChanged();
 		this.notifyObservers(launchable);
+	}
+	
+	private void exportFileList(File[] files) {
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(exportFile));
+			for (File f : files) {
+				System.out.println(f.getName());
+				bw.write(f.getName());
+				bw.newLine();
+			}
+			bw.flush();
+			bw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
